0b6a12ec1c0edc3faed89dae3cef68a2d93c84a3
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2019-09-29 17:33:01 +0200

    add unit test for isOutOfArea

---
+++ src/test/java/com/mowitnow/lawnowner/models/AreaTests.java
@@ -0,0 +1,24 @@
+package com.mowitnow.lawnowner.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AreaTests {
+
+    @Test
+    public void testIsOutOfArea(){
+        Coordinate c1 = new Coordinate(-1, 2);
+        Coordinate c2 = new Coordinate(2, 2);
+        Coordinate c3 = new Coordinate(7, 2);
+        Coordinate c4 = new Coordinate(3, 6);
+
+        Area a1 = new Area(5,6);
+
+        assertTrue(a1.isOutOfArea(c1));
+        assertFalse(a1.isOutOfArea(c2));
+        assertTrue(a1.isOutOfArea(c3));
+        assertFalse(a1.isOutOfArea(c4));
+    }
+}

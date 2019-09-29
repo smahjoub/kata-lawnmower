9018dce23707322b2a19c01a3b70c69e99468527
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2019-09-29 16:50:05 +0200

    add unit tests for direction rotation

---
+++ src/test/java/com/mowitnow/lawnowner/models/DirectionTests.java
@@ -0,0 +1,25 @@
+package com.mowitnow.lawnowner.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DirectionTests {
+
+    @Test
+    public void testRotationLeft(){
+
+        assertTrue(Direction.N.getRotationLeftResult() == Direction.W);
+        assertTrue(Direction.W.getRotationLeftResult() == Direction.S);
+        assertTrue(Direction.S.getRotationLeftResult() == Direction.E);
+        assertTrue(Direction.E.getRotationLeftResult() == Direction.N);
+    }
+
+    @Test
+    public void testRotationRight(){
+        assertTrue(Direction.N.getRotationLeftResult() == Direction.E);
+        assertTrue(Direction.E.getRotationLeftResult() == Direction.S);
+        assertTrue(Direction.S.getRotationLeftResult() == Direction.W);
+        assertTrue(Direction.W.getRotationLeftResult() == Direction.N);
+    }
+}

6e92747319dacec3caadba7ca1fbbaffa2886bce
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2019-09-29 18:29:44 +0200

    Implement execute methods

--- src/main/java/com/mowitnow/lawnowner/models/LawnField.java
+++ src/main/java/com/mowitnow/lawnowner/models/LawnField.java
@@ -1,6 +1,8 @@
 package com.mowitnow.lawnowner.models;
 
+import com.mowitnow.lawnowner.events.ExecutionCompletedEvent;
 import com.mowitnow.lawnowner.events.ExecutionCompletedEventListener;
+import javafx.util.Pair;
 
 import java.util.List;
 import java.util.Map;
@@ -16,8 +18,51 @@ public class LawnField {
 
     public void execute(Map<LawnOwner, List<Command>> process){
 
+        for (Map.Entry<LawnOwner, List<Command>> me: process.entrySet()) {
+            final LawnOwner currentLawnOwner = me.getKey();
+            for(Command command: me.getValue()){
+                final Pair<Coordinate, Direction> commandResult = executeCommand(currentLawnOwner,command);
+                final Coordinate nextCoordinate = commandResult.getKey();
+                // check if the next coordinate is inside the given area
+                if(!area.isOutOfArea(nextCoordinate)){
+                    // Update position and direction
+                    currentLawnOwner.setCurrentPosition(commandResult.getKey());
+                    currentLawnOwner.setCurrentDirection(commandResult.getValue());
+                }
+            }
+
+            if(executionCompletedEventListener != null){
+                executionCompletedEventListener
+                        .onExecutionCompletedEvent(new ExecutionCompletedEvent(
+                                currentLawnOwner,
+                                currentLawnOwner.getCurrentPosition(),
+                                currentLawnOwner.getCurrentDirection()));
+            }
+        }
 
     }
+
+
+    private Pair<Coordinate, Direction> executeCommand(LawnOwner l, Command c) {
+        Coordinate coordinate = new Coordinate(l.getCurrentPosition().getX(),
+                l.getCurrentPosition().getY());
+        Direction direction = l.getCurrentDirection();
+        switch (c){
+            case A:
+                coordinate.move(l.getCurrentDirection());
+                break;
+            case D:
+                direction = direction.getRotationRightResult();
+                break;
+            case G:
+                direction = direction.getRotationLeftResult();
+                break;
+
+        }
+
+        return new Pair<>(coordinate, direction);
+    }
+
 
     public void setExecutionCompletedEventListener(ExecutionCompletedEventListener executionCompletedEventListener) {
         this.executionCompletedEventListener = executionCompletedEventListener;

4ff2cc839d734ecbe757337ce3fc332882503c57
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2019-09-29 18:14:11 +0200

    add lawnOwner attribute

--- src/main/java/com/mowitnow/lawnowner/events/ExecutionCompletedEvent.java
+++ src/main/java/com/mowitnow/lawnowner/events/ExecutionCompletedEvent.java
@@ -2,16 +2,25 @@ package com.mowitnow.lawnowner.events;
 
 import com.mowitnow.lawnowner.models.Coordinate;
 import com.mowitnow.lawnowner.models.Direction;
+import com.mowitnow.lawnowner.models.LawnOwner;
 
 public class ExecutionCompletedEvent {
+
+    private LawnOwner lawnOwner;
 
     private Coordinate finalCoordinate;
 
     private Direction finalDirection;
 
-    public ExecutionCompletedEvent(Coordinate finalCoordinate, Direction finalDirection) {
+    public ExecutionCompletedEvent(LawnOwner lawnOwner,
+                                   Coordinate finalCoordinate, Direction finalDirection) {
+        this.lawnOwner = lawnOwner;
         this.finalCoordinate = finalCoordinate;
         this.finalDirection = finalDirection;
+    }
+
+    public LawnOwner getLawnOwner() {
+        return lawnOwner;
     }
 
     public Coordinate getFinalCoordinate() {
